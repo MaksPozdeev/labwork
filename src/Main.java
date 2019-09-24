60ff9a1c7c599c815c15ef17b7884dd1e7d0cefe
Maksim Pozdeev <maximglazov18@gmail.com>
2019-09-24 14:02:59 +0400

    Second commit to github with ssh not IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program without ssh");
+        System.out.println("Program with ssh bou not IDEA");
     }
 }
