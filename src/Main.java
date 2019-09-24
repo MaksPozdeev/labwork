4b8d7eab61cf12a41d720a7d4e8117b149b3cf02
Maksim <maximglazov18@gmail.com>
2019-09-24 16:59:07 +0400

    Commit #4 from GitKraken

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Program with ssh bou not IDEA 2");
+        System.out.println("All work???");
     }
 }
